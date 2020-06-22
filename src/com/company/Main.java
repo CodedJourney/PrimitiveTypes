ec6bb6c8b4bb728cb506081b00243c0d4173565a
Coded Journey <coded.journey@gmail.com>
2020-06-22 16:22:52 -0700

    Finding min and max value of int, and learning underflow and overflow

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // Testing min and max values of int
+        // Using wrapper class to find min and max values of int
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
+        // Example of underflow
+        System.out.println("Busted Min Value = " + (myMinIntValue - 1));
+        // Example of overflow
+        System.out.println("Busted Max Value = " + (myMaxIntValue + 1));
     }
 }
